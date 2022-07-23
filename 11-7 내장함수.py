9b5fe61967c5ce71e3ed947afc8a7dc34afa434b
ThovenhairWorrior <106158869+ThovenhairWorrior@users.noreply.github.com>
2022-07-23 21:51:42 +0900

    Add files via upload

---
+++ 11-7 내장함수.py
@@ -0,0 +1,14 @@
+# input : 사용자 입력을 받는 함수
+#language = input("무슨 언어를 좋아하시나요? ")
+#print("{0}은 아주 좋은 언어입니다!".format(language))
+
+# dir: 어떤 객체를 넘겨줬을때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+#print(dir())
+import random # 외장함수
+#print(dir())
+#import pickle # 외장함수
+#print(dir())
+print(dir(random))
+
+# list of python builtins : 파이썬에서 쓸 수 있는 내장함수를 찾을 수 있음
+
